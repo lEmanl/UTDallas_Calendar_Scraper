1b0f20c11bb8eff28dc30fd702cd589677351464
Emanuel Urbina <emanuelurbina@live.com>
2019-02-24 20:11:32 -0600

    made url string constant

--- web_scrap.js
+++ web_scrap.js
@@ -18,10 +18,13 @@ event (#event-detail .cat-events.thirty-type)
 
 //  export of webScraper function with callback
 exports.webScraper = function (callback) {
+    
+    const url = "https://www.utdallas.edu/calendar/mobile/events-by-date.php?type=thirty"
+
     /*
         Gets our html data from utdallas calendar website
     */
-    https.get("https://www.utdallas.edu/calendar/mobile/events-by-date.php?type=thirty", (res) => {
+    https.get(url, (res) => {
 
         //  holds html data from website
         let data = ''
@@ -30,7 +33,7 @@ exports.webScraper = function (callback) {
         res.on('data', (chunk) => {
             data += chunk
         })
-
+        
         //  on end response, save relevant html data into object 'events'
         res.on('end', () => {
 
@@ -58,11 +61,13 @@ exports.webScraper = function (callback) {
 
             //  if successfully got data, then callback function used with events object passed to it
             return callback(events)
+
         })
     }).on("error", (err) => {
         console.log("Error: " + err.message)
 
         //  return error if error on get request
         return "err"
+
     })
 }
